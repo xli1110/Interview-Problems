739c584c38c214c8c92970bc8aca9c90124aa638
lixuyang05 <lixuyang05@meituan.com>
2021-03-24 17:32:14 +0800

    N/A

---
+++ Microsoft - SDE/Interview - 1.py
@@ -0,0 +1,50 @@
+"""
+Date: 2021 03 19
+Problems: CV + Code + Machine Learning
+Result: Fail
+"""
+
+
+class Problem1:
+    """
+    Machine Learning
+
+    1 - Tree
+    classification, regression
+
+    2 - Ensemble Learning
+    loss function
+
+    3 - Pick the most familiar model.(Linear Regression)
+    When is MSE the best depending on labels' distribution?
+    """
+
+
+class Problem2:
+    """
+    Binary Search
+    LeetCode - 35
+    Interviewed on 9 p.m. Friday.
+    Tired AF, then wrote code below with linear iteration not logarithmic.
+    """
+
+    def insert_target(self, x, t):
+        if not x:
+            return 0
+
+        low = 0
+        high = len(x) - 1
+
+        while low < high:
+            mid = (low + high) >> 1
+            if x[mid] == t:
+                return mid
+            elif x[mid] < t:
+                high -= 1  # I suck
+            else:
+                low += 1  # same as above
+
+        if x[low] > t:
+            return low
+        else:
+            return low + 1
